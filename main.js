e4d942142ad04776da5c5cf385edd859520b1d6e
alexbakhu <aleqsandrekepler123@gmail.com>
2025-03-15 14:00:59 +0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,92 @@
+let num1 = 1
+
+let num2 = 9
+
+let num3 = 7
+
+console.log(num1)
+console.log(num2)
+console.log(num3)
+
+let num4 = 90 * 12
+
+let num5 = 9 - 7
+
+let num6 =  80 / 10
+
+let num7 = 900 + 100
+
+
+console.log(num4)
+console.log(num5)
+console.log(num6)
+console.log(num7)
+
+num2 = 500
+
+const name1 = "bakhu"
+
+const color1 = "black"
+
+const name2 = "josh"
+
+const color2 = "grey"
+
+
+console.log(name1)
+console.log(color1)
+console.log(name2)
+console.log(color2)
+
+console.log(color1.length)
+
+ let p = document.querySelector(".p1")
+
+ p.style.color = "blue"
+ p.style.backgroundColor = "green"
+ p.style.border = '5px dashed red'
+ p.style.padding = '20px'
+
+ let button= document.querySelector(".btn1")
+
+ button.style.color = "red"
+ button.style.backgroundColor = "blue"
+ button.style.border = '10px dashed purple'
+ button.style.padding = '5px'
+
+ p.textContent = "hi! :D"
+
+button.addEventListener( "click", () => {
+  console.log('hellloooooo')
+})
+
+button.addEventListener( "click", () => {
+    console.log('hellloooooo')
+  })
+  //davalebaaaaaaaaa
+
+  let button2= document.querySelector(".red")
+  let button3= document.querySelector(".blue")
+  let button4= document.querySelector(".green")
+  let button5= document.querySelector(".yellow")
+  let body = document.querySelector(".body1")
+  
+  button2.addEventListener( "click", () => {
+   body.style.backgroundColor = "red"
+  button2.textContent = 'button was clicked'
+})
+
+  button3.addEventListener( "click", () => {
+    body.style.backgroundColor = "blue"
+   button3.textContent = 'button was clicked'
+})
+
+   button4.addEventListener( "click", () => {
+    body.style.backgroundColor = "green"
+    button4.textContent = 'button was clicked'
+})
+
+   button5.addEventListener( "click", () => {
+    body.style.backgroundColor = "yellow"
+     button5.textContent = 'button was clicked'
+})
